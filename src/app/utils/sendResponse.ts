732245934c7648a3dc8c3e2baeef8f913b0a9264
SUMON <belalhossain22000@gmail.comgit config --global user.name>
2023-12-15 00:40:56 +0600

    aded meta data formate

--- src/app/utils/sendResponse.ts
+++ src/app/utils/sendResponse.ts
@@ -14,6 +14,7 @@ const sendResponse = <T>(res: Response, data: TResponse<T>) => {
         success: data.success,
         statusCode: data.statusCode,
         message: data.message,
+        meta: data.meta,
         data: data.data,
     };
 
